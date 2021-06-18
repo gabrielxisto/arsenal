930062416ed4766289b60b9aa67551130d84913b
Gabriel Xisto <71442950+Alzon22@users.noreply.github.com>
2021-06-18 01:59:01 -0300

    Create xstserver.lua

---
+++ xstserver.lua
@@ -0,0 +1,23 @@
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP")
+
+vRPex = {}
+Tunnel.bindInterface("vrp_arsenal",vRPex)
+
+function vRPex.checkPermission(permission)
+	local src = source
+    local user_id = vRP.getUserId(src)
+    if user_id then
+        return vRP.hasPermission(user_id,permission)
+    end
+end
+
+function vRPex.checkOfficer()
+	local src = source
+	local user_id = vRP.getUserId(source)
+	if vRP.hasPermission(user_id,"policia.permissao") or vRP.hasPermission(user_id,"policia-acao.permissao") then
+		return true
+	end
+end
