fd2982b11d8478a77a2195dd6121af34dd56a014
Gabriel Xisto <71442950+Alzon22@users.noreply.github.com>
2021-07-28 17:25:42 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'Xisto'
+description 'One simple arsenal for FiveM with utilze commands devveloped by Xisto'
+version '1.0.0'
+
 client_scripts {
 	'@vrp/lib/utils.lua',
 	'xstclient.lua'
